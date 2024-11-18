7c83b3b496ec3b49e7b9329bea8a171c203cf05e
Luis Carlos <luiscbravo94@gmail.com>
2024-11-18 22:09:31 +0000

    Project finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { useRef,useState } from 'react';
 import './App.css'
 
 function App() {
+  const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
+  const [inputValue,setInputValue] = useState('')
+  const colorName = useRef('')
+  {console.log(colorName.current)}
   return (
     <>
+    
+    <input 
+    type='text' 
+    onChange={(e) => setInputValue(e.target.value)} 
+    ref={colorName} 
+    value={inputValue}  
+
+    ></input>
+    <div className='container'>
+      
+      {colors.map((color,index) => <div key={index} className='box' style={ colorName.current.value !== color ?  {border:`1px solid ${color}`, color:`${color}`} : {border:`1px solid ${color}`, color:'white' , background:`${color}`}  } > {colorName.current.value !== color ? `I'm not a ${colorName.current.value} color` : `I'm a ${colorName.current.value} color`  } </div>)}
+
+    </div>
     </>
   )
 }
